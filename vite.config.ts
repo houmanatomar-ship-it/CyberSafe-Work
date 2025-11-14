e17c972ff0dc365bcb044f801d19622b4bb1a620
houmanatomar-ship-it <houmanatomar@gmail.com>
2025-11-14 16:29:28 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+export default defineConfig({
+  plugins: [react()],
+  base: './', // IMPORTANT pour Vercel
+});
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
